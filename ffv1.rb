659bc1df3798516536f65cd9297bec2c8c866ec4
mdhamija <mdhamija94@gmail.com>
2019-08-11 12:24:09 -0700

    Polished output and abstracted list of participants to text file

--- ffv1.rb
+++ ffv1.rb
@@ -1,5 +1,5 @@
-apartment_list = %w(Mohit Laura Evan Sue Gabby Bre Max John Chris Jax Frida Kelsey Jamiah Liz Ryan Victor Ian Calvin Benson Jack KK Aitor Chee Lauren Priya JZ)
-
+people = File.readlines("apartment_list.txt").map(&:chomp)
+apartment_list = Array.new(people)
 
 def fam_fri(people)
     groups = group_builder(people)
@@ -35,6 +35,6 @@ puts "Participant Count: #{apartment_list.length}"
 
 groups = fam_fri(apartment_list)
 
-groups.each do |group|
-    puts "#{group}"
+groups.each_with_index do |group, i|
+    puts "Group #{i + 1}: #{group}"
 end
